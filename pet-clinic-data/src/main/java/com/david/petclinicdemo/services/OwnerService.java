9bb54ebeaa231f972f2c4f729e28f337a7090f43
david.hsu <david.hsu@mtaxi.com.tw>
2022-07-01 15:16:40 +0800

    refactor Services using CrudService to reduce duplicate code

--- pet-clinic-data/src/main/java/com/david/petclinicdemo/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/david/petclinicdemo/services/OwnerService.java
@@ -2,16 +2,8 @@ package com.david.petclinicdemo.services;
 
 import com.david.petclinicdemo.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastname);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 
 }
--- pet-clinic-data/src/main/java/com/david/petclinicdemo/services/PetService.java
+++ pet-clinic-data/src/main/java/com/david/petclinicdemo/services/PetService.java
@@ -2,14 +2,7 @@ package com.david.petclinicdemo.services;
 
 import com.david.petclinicdemo.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 
 }
